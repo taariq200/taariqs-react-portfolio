abbb64ed8c261179a3f6ce4dfcaf62e75f31e02d
taariq200 <tglean1@student.gsu.edu>
2023-07-28 11:29:44 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
+
 import './App.css';
 
+import { useState } from "react";
+
+const Hi = (props) => {
+  const {num} = props;
+  return (
+    <div>Hi. {num}</div>
+  )
+}
 function App() {
+
+const [number, setNumber] = useState(0);
+
+const add = () => {
+  setNumber(number + 1)
+}
+const reset = () => {
+  setNumber(0)
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Hi num = {number}/>
+      <div>Number = {number}</div>
+      <button onClick = {add}>Add</button>
+      <button onClick = {reset}>Reset</button>
     </div>
   );
 }
